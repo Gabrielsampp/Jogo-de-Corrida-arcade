cafdd7d6441932d5e52137487fd0ee835d050da8
Angel01331 <wellington.dantas@aluno.ufca.edu.br>
2026-08-06 17:53:39 -0300

    Criação da função update para os dados do jogador.

---
+++ Entidades/Jogador/update.py
@@ -0,0 +1,34 @@
+#FUNÇÂO UPDATE - JOGADOR
+
+import json
+import os
+
+# Este comando abaixo descobre o endereço do arquivo jogador.json pela lógica de um "caminho relativo"
+# Utilizamos pois o caminho absoluto, como aprendemos com o Wesckley, pode mudar de acordo com o SO
+
+CAMINHO = os.path.join(os.path.dirname(__file__), "jogador.json")
+
+# Função update:
+
+def atualizar_jogador(nickname, novos_dados):
+
+    #abre arquivo para leitura e atribui os dados do arquivo a variável dados, tornando-a como um "dicionário".
+
+    with open(CAMINHO, "r", encoding="utf-8") as arquivo:
+        dados = json.load(arquivo)
+
+    # Verifica se é o jogador e subistitui os dados do dicionário com as diferenças da segunda entrada da função.
+
+    for jogador in dados["jogadores"]:
+        if jogador["nickname"] == nickname:
+
+            jogador.update(novos_dados)
+
+            #abre o arquivo para escrita e sobrescreve com os dados que foram modificados acima
+
+            with open(CAMINHO, "w", encoding="utf-8") as arquivo:
+                json.dump(dados, arquivo, indent=4, ensure_ascii=False)
+
+            return True
+
+    return False
