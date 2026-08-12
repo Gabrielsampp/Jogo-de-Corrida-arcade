04c67d697555b61280a4af447c095fc2c0ee990a
Angel01331 <wellington.dantas@aluno.ufca.edu.br>
2026-08-12 16:32:28 -0300

    Concluindo o pista.py e aprimoramento  dos comentários.

--- Entidades/Pista/pista.py
+++ Entidades/Pista/pista.py
@@ -2,20 +2,20 @@
 
 from create import cadastrar_pista
 from delete import delete
-from get_all import ...
+from get_all import get_all
 from get_by_list_id import buscar_pistas_por_id
 from get_last_id import get_last_id
 from get_publics import get_publics
 from update import update
 
 # Define o que fica disponível ao importar o módulo. 
-# Os módulos importantes ficam disponíveis.
-# Os módulos auxiliares ficam protejidos.
+# As funções importantes ficam disponíveis.
+# As ferramentas auxiliares ficam protejidos.
 
 __all__ = [
     "cadastrar_pista",
     "delete",
-    "...",
+    "get_all",
     "buscar_pistas_por_id",
     "get_last_id",
     "get_publics",
