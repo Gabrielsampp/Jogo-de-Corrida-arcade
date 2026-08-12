e07664cfa95dd59fcf25e56ab229cd48945a813e
José Alisson <josealissondiasdacostaf10@gmail.com>
2026-08-12 19:12:02 -0300

    Atualizando Pista/pista.py

--- Entidades/Pista/pista.py
+++ Entidades/Pista/pista.py
@@ -1,23 +1,10 @@
 # Funções importadas
 
-from create import cadastrar_pista
-from delete import delete
-from get_all import get_all
-from get_by_list_id import buscar_pistas_por_id
-from get_last_id import get_last_id
-from get_publics import get_publics
-from update import update
+from Entidades.Pista.create import cadastrar_pista as create
+from Entidades.Pista.delete import delete
+from Entidades.Pista.get_all import get_all
+from Entidades.Pista.get_by_list_id import buscar_pistas_por_id as get_by_list_id
+from Entidades.Pista.get_last_id import get_last_id
+from Entidades.Pista.get_publics import get_publics
+from Entidades.Pista.update import update
 
-# Define o que fica disponível ao importar o módulo. 
-# As funções importantes ficam disponíveis.
-# As ferramentas auxiliares ficam protejidos.
-
-__all__ = [
-    "cadastrar_pista",
-    "delete",
-    "get_all",
-    "buscar_pistas_por_id",
-    "get_last_id",
-    "get_publics",
-    "update",
-]
