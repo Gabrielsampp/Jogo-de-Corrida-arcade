2c9bb0aa842d26b15d7dbae915cddd84557ea828
CaioOliveira-456 <caiooliveirasa81@gmail.com>
2026-08-14 15:32:12 -0300

    Comentários remove_player

--- Entidades/Ranking/remove_player.py
+++ Entidades/Ranking/remove_player.py
@@ -4,17 +4,17 @@ from pathlib import Path
 caminho = Path(__file__).parent / "ranking.json"
 
 
-def remove_player(nome):
+def remove_player(nome):                                    #função para remover player do ranking
     with open(caminho, "r", encoding="utf-8") as arquivo:
         dados = json.load(arquivo)
 
     ranking = dados["ranking"]
 
-    for jogador in ranking:
+    for jogador in ranking:                                         #busca o nome do jogador no ranking, se encontrar, ele remove do ranking
         if jogador["nome"] == nome:
             ranking.remove(jogador)
 
-            with open(caminho, "w", encoding="utf-8") as arquivo:
+            with open(caminho, "w", encoding="utf-8") as arquivo:               #alteração do arquivo
                 json.dump(dados, arquivo, indent=4, ensure_ascii=False)
 
             return True, "Jogador removido do ranking"
