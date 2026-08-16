62aae48996e9268a78635e005da118126f95fd99
Angel01331 <wellington.dantas@aluno.ufca.edu.br>
2026-08-16 10:48:58 -0300

    Melhorias para testes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Jogo.tela_ranking import tela_ranking
-from Entidades.createPista.create import cadastrar_pista
+from Entidades.Pista.create import cadastrar_pista
 
 cadastrar_pista()
+tela_ranking()
