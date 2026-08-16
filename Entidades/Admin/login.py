691923a81fbd8408179cc91a5bf6552bd2c5bddd
CaioOliveira-456 <caiooliveirasa81@gmail.com>
2026-08-16 17:15:03 -0300

    Função do menu principal

--- Entidades/Admin/login.py
+++ Entidades/Admin/login.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 path = Path(__file__).parent / "admin.json" #Busca o arquivo do admin.json
 
-def login(username, password):                           #Função criada para testar a validação do admin         
+def login_admin(username, password):                           #Função criada para testar a validação do admin         
     with open(path, "r", encoding="utf-8") as file:
         data = json.load(file)
 
